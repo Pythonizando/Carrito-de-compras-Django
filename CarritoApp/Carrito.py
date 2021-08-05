f20fcb73261aea0d11335987b02b8ca7a61eee04
Pythonizando <88479338+Pythonizando@users.noreply.github.com>
2021-08-05 02:22:54 -0500

    Add files via upload

---
+++ CarritoApp/Carrito.py
@@ -0,0 +1,46 @@
+class Carrito:
+    def __init__(self, request):
+        self.request = request
+        self.session = request.session
+        carrito = self.session.get("carrito")
+        if not carrito:
+            self.session["carrito"] = {}
+            self.carrito = self.session["carrito"]
+        else:
+            self.carrito = carrito
+
+    def agregar(self, producto):
+        id = str(producto.id)
+        if id not in self.carrito.keys():
+            self.carrito[id]={
+                "producto_id": producto.id,
+                "nombre": producto.nombre,
+                "acumulado": producto.precio,
+                "cantidad": 1,
+            }
+        else:
+            self.carrito[id]["cantidad"] += 1
+            self.carrito[id]["acumulado"] += producto.precio
+        self.guardar_carrito()
+
+    def guardar_carrito(self):
+        self.session["carrito"] = self.carrito
+        self.session.modified = True
+
+    def eliminar(self, producto):
+        id = str(producto.id)
+        if id in self.carrito:
+            del self.carrito[id]
+            self.guardar_carrito()
+
+    def restar(self, producto):
+        id = str(producto.id)
+        if id in self.carrito.keys():
+            self.carrito[id]["cantidad"] -= 1
+            self.carrito[id]["acumulado"] -= producto.precio
+            if self.carrito[id]["cantidad"] <= 0: self.eliminar(producto)
+            self.guardar_carrito()
+
+    def limpiar(self):
+        self.session["carrito"] = {}
+        self.session.modified = True
---
+++ CarritoApp/context_processor.py
@@ -0,0 +1,7 @@
+def total_carrito(request):
+    total = 0
+    if request.user.is_authenticated:
+        if "carrito" in request.session.keys():
+            for key, value in request.session["carrito"].items():
+                total += int(value["acumulado"])
+    return {"total_carrito": total}
