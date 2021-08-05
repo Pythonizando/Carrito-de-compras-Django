dbee694d3ee70ab75c9b7410252dd3e01a350094
Pythonizando <88479338+Pythonizando@users.noreply.github.com>
2021-08-05 02:24:34 -0500

    Add files via upload

---
+++ CarritoApp/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Producto(models.Model):
+    nombre = models.CharField(max_length=64)
+    categoria = models.CharField(max_length=32)
+    precio = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.nombre} -> {self.precio}'
---
+++ CarritoApp/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, HttpResponse, redirect
+
+# Create your views here.
+from CarritoApp.Carrito import Carrito
+from CarritoApp.models import Producto
+
+
+def tienda(request):
+    #return HttpResponse("Hola Pythonizando")
+    productos = Producto.objects.all()
+    return render(request, "tienda.html", {'productos':productos})
+
+def agregar_producto(request, producto_id):
+    carrito = Carrito(request)
+    producto = Producto.objects.get(id=producto_id)
+    carrito.agregar(producto)
+    return redirect("Tienda")
+
+def eliminar_producto(request, producto_id):
+    carrito = Carrito(request)
+    producto = Producto.objects.get(id=producto_id)
+    carrito.eliminar(producto)
+    return redirect("Tienda")
+
+def restar_producto(request, producto_id):
+    carrito = Carrito(request)
+    producto = Producto.objects.get(id=producto_id)
+    carrito.restar(producto)
+    return redirect("Tienda")
+
+def limpiar_carrito(request):
+    carrito = Carrito(request)
+    carrito.limpiar()
+    return redirect("Tienda")
